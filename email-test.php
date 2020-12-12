c760fa933ff75c818208cbab58b74cdba8e07178
Frank Corso <frank@frankcorso.me>
2020-12-12 18:43:11 -0500

    Outline main plugin class

--- email-test.php
+++ email-test.php
@@ -16,4 +16,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+if ( ! class_exists( 'SA_Email_Test' ) ) {
+	/**
+	 * The plugin's main class
+	 *
+	 * @since 0.1.0
+	 */
+	class SA_Email_Test {
+
+		public static function init() {
+			self::load_dependencies();
+			self::load_hooks();
+		}
+
+		public static function load_dependencies() {
+
+		}
+
+		public static function load_hooks() {
+
+		}
+	}
+
+	add_action( 'plugins_loaded', array( 'SA_Email_Test', 'init' ) );
+}
 ?>
