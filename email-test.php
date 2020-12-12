ec219f9afeda93215e46cc7def7ef9759935d07f
Frank Corso <frank@frankcorso.me>
2020-12-12 18:32:03 -0500

    Create main plugin file

---
+++ email-test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Email Test
+ * Description: Test your site's email to make sure emails are being sent.
+ * Version: 0.1.0
+ * Author: Frank Corso
+ * Author URI: https://frankcorso.me
+ * Plugin URI: https://frankcorso.me
+ * Text Domain: email-test
+ *
+ * @author Frank Corso
+ */
+
+// Exits if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
